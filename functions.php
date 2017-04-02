3e06017fe90d38eb5f5796653021341532d3202a
Dan Mon <Dan Mon>
2017-04-02 12:08:35 -0500

    add custom header image

--- functions.php
+++ functions.php
@@ -19,6 +19,22 @@ register_nav_menus( array(
 	));
 
 
+/* Custom Header Feature */
+
+function clean_custom_header() {
+	add_theme_support( 'custom-header', apply_filters( 'clean_custom_header_args', array(
+		'default-image'          => get_template_directory_uri() . '/img/home-bg.jpg',
+		'default-text-color'     => '000000',
+		'width'                  => 1900,
+		'height'                 => 872,
+		'flex-height'            => true,
+		'uploads' => true,
+		'wp-head-callback'       => '',
+	) ) );
+}
+add_action( 'after_setup_theme', 'clean_custom_header' );
+
+
 // Registro de css y script
 
 function reg_script() {
