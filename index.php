2e87db6f2fc768452dcfc1dd566f5dec6ca93e63
Dan Mon <Dan Mon>
2017-04-02 12:17:46 -0500

    add subheading in index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                         <h2 class="post-title"><?php the_title(); ?>
                         </h2>
                         <h3 class="post-subtitle">
-                            <?php the_excerpt(); ?>
+                            <?php the_subtitle(); ?>
                         </h3>
                     </a>
                     <p class="post-meta">Posted by <a href="#"><?php the_author(); ?></a> <?php echo " " .get_the_date(); ?></p>
